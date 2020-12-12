2b012211a53f352fe6f46f1ffcc4910388dcd4a7
Windoof | Maximilian <57870403+Windoof-Colourblindoof@users.noreply.github.com>
2020-12-12 22:05:36 +0100

    Added Main Python

---
+++ Lichess-PGNFormatter.py
@@ -0,0 +1,18 @@
+from pip._internal import main as pipmain
+
+pipmain(['install', 'python-lichess'])
+
+import lichess.api
+from lichess.format import SINGLE_PGN
+from datetime import datetime
+import re
+
+User = 'RestaurantB'
+
+DATET = datetime.now()
+now = DATET.strftime("%m.%d.%Y %H;%M;%S")
+pgn = lichess.api.user_games(User, max=1, format=SINGLE_PGN)
+pgnNew = re.sub(r'([0-9]+\.+\s)', '\n\\1', pgn)
+
+with open(now + '.pgn', 'w') as f:
+   f.write(pgnNew)
